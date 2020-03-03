87705cb58fb41913da1649db279ee2098defd56c
Aman <amancse111@gmail.com>
2020-03-04 00:01:07 +0530

    MY FIRST COMMIT

---
+++ src/com/app/Test.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+	public static void main(String[] args) {
+        Map<Integer, String> m= new HashMap<>();
+        m.put(7, "aman");
+        m.put(2, "raman");
+        m.put(3, "rajan");
+        System.out.println(m);
+        
+	}
+
+}
